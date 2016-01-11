2eb926794d5f8a8d4637af04a871d44dd5d7df03
inuscript <inuscript@gmail.com>
2016-01-11 19:45:38 +0900

    split layer

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,42 +1,7 @@
 import React, { Component, PropTypes } from "react"
 import ReactDOM from "react-dom"
 import { PixelBox } from "../lib/pixel"
-
-class Layer extends Component{
-  static propTypes(){
-    return {
-      index: PropTypes.number.isRequired,
-      width: PropTypes.number.isRequired,
-      height: PropTypes.number.isRequired,
-      onPaint: PropTypes.func
-    }
-  }
-  componentDidMount(){
-    let context = this.getContext()
-    if(!this.props.onPaint){
-      return
-    }
-    this.props.onPaint(context)
-  }
-  componentDidUpdate() {
-    let context = this.getContext()
-    if(!this.props.onPaint){
-      return
-    }
-    this.props.onPaint(context);
-  }
-  getContext(){
-    return ReactDOM.findDOMNode(this).getContext('2d')
-  }
-  clear(context){
-    context.clearRect(0, 0, this.props.width, this.props.height);
-  }
-  render(){
-    let style = {position: "absolute", zIndex: this.props.index}
-    // let { width, height } = this.props
-    return <canvas style={style} {...this.props} />
-  }
-}
+import { Layer } from "./Layer"
 
 class ContextRender{
   constructor(context, pixSize = 10){
